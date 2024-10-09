4442407675e83d1e7bcee5214bc2cf55814b8b2e
tbxark <tbxark@outlook.com>
2024-10-09 10:36:13 +0800

    fix: 章节排序逻辑

--- main.go
+++ main.go
@@ -1,39 +1,47 @@
 package main
 
 import (
-	"bytes"
 	"flag"
 	"fmt"
 	"github.com/PuerkitoBio/goquery"
 	"golang.org/x/text/encoding/simplifiedchinese"
-	"io"
 	"log"
 	"net/http"
 	"os"
-	"sort"
+	"path/filepath"
 	"strconv"
 	"strings"
 )
 
+const (
+	UserAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.0.1 Safari/605.1.15"
+	BaseURL   = "https://69shuba.cx"
+)
+
 func main() {
-	id := flag.Int("id", 0, "id of the books")
-	file := flag.String("file", "book.txt", "path to save the books")
+	id := flag.Int("id", 29624, "id of the books")
+	dir := flag.String("dir", "./", "directory to save the books")
 	flag.Parse()
 	if *id == 0 {
 		log.Panic("Please provide the id of the books")
 	}
-	category, err := parseCategory(*id)
+	title, category, err := parseCategory(*id)
 	if err != nil {
 		log.Panic(err)
 	}
-	save, err := os.Create(*file)
+	file := filepath.Join(*dir, title+".txt")
+	save, err := os.Create(file)
 	if err != nil {
 		log.Panic(err)
 	}
 	defer save.Close()
 	for _, book := range category {
+		if !strings.HasPrefix(book, "http") {
+			continue
+		}
 		log.Printf("Downloading %s", book)
 		title, content, e := parseBook(book)
+		log.Printf("Downloaded %s", title)
 		if e != nil {
 			log.Panic(e)
 		}
@@ -49,44 +57,64 @@ func main() {
 	log.Println("Downloaded all books")
 }
 
-func parseCategory(id int) ([]string, error) {
-	req, err := http.NewRequest(http.MethodGet, "https://69shuba.cx/book/"+strconv.Itoa(id)+"/", nil)
+func parseCategory(id int) (title string, category []string, err error) {
+	req, err := http.NewRequest(http.MethodGet, BaseURL+"/book/"+strconv.Itoa(id)+"/", nil)
 	if err != nil {
-		return nil, err
+		return
 	}
-	req.Header.Set("User-Agent", " Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.0.1 Safari/605.1.15")
+	req.Header.Set("User-Agent", UserAgent)
 	res, err := http.DefaultClient.Do(req)
 	if err != nil {
-		return nil, err
+		return
 	}
 
 	defer res.Body.Close()
 	if res.StatusCode != 200 {
-		return nil, fmt.Errorf("status code error: %d %s", res.StatusCode, res.Status)
+		err = fmt.Errorf("status code error: %d %s", res.StatusCode, res.Status)
+		return
 	}
 
-	doc, err := goquery.NewDocumentFromReader(res.Body)
+	doc, err := goquery.NewDocumentFromReader(simplifiedchinese.GB18030.NewDecoder().Reader(res.Body))
 	if err != nil {
-		return nil, err
+		return
 	}
-	var booksLinks []string
+	var minNum, maxNum int
+	booksLinks := make(map[int]string)
 	doc.Find(".catalog ul li").Each(func(i int, s *goquery.Selection) {
-		link, _ := s.Find("a").Attr("href")
-		if strings.HasPrefix(link, "http") {
-			booksLinks = append(booksLinks, link)
+		link, ok := s.Find("a").Attr("href")
+		if !ok {
+			return
 		}
+		dataNum, ok := s.Attr("data-num")
+		if !ok {
+			return
+		}
+		num, e := strconv.Atoi(dataNum)
+		if e != nil {
+			return
+		}
+		if num < minNum {
+			minNum = num
+		}
+		if num > maxNum {
+			maxNum = num
+		}
+		booksLinks[num] = link
 	})
-	sort.Sort(sort.StringSlice(booksLinks))
-	return booksLinks, nil
+	category = make([]string, 0, maxNum-minNum+1)
+	for i := minNum; i <= maxNum; i++ {
+		category = append(category, booksLinks[i])
+	}
+	title = doc.Find(".bread a").Last().Text()
+	return
 }
 
 func parseBook(link string) (title, content string, err error) {
-	title, content = "", ""
 	req, err := http.NewRequest(http.MethodGet, link, nil)
 	if err != nil {
 		return
 	}
-	req.Header.Set("User-Agent", " Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.0.1 Safari/605.1.15")
+	req.Header.Set("User-Agent", UserAgent)
 	res, err := http.DefaultClient.Do(req)
 	if err != nil {
 		return
@@ -96,19 +124,7 @@ func parseBook(link string) (title, content string, err error) {
 	if res.StatusCode != 200 {
 		return
 	}
-
-	body, err := io.ReadAll(res.Body)
-	if err != nil {
-		return
-	}
-
-	// 将 GBK 或 GB18030 转换为 UTF-8
-	utf8Body, err := simplifiedchinese.GB18030.NewDecoder().Bytes(body) // 或使用 simplifiedchinese.GBK
-	if err != nil {
-		panic(err)
-	}
-
-	doc, err := goquery.NewDocumentFromReader(bytes.NewReader(utf8Body))
+	doc, err := goquery.NewDocumentFromReader(simplifiedchinese.GB18030.NewDecoder().Reader(res.Body))
 	if err != nil {
 		return
 	}
